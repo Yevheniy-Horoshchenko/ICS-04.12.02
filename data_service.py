e6a99aacb99920f7896f58f5edde1dae7c87aa72
Yevheniy-Horoshchenko <horoshchenko57@gmail.com>
2020-11-17 10:17:03 +0200

    commit

--- data_service.py
+++ data_service.py
@@ -21,7 +21,7 @@ def get_products():
     """повертає вміст файла 'products.txt' у вигляді списка
     """
  
-    with open ('./data/products.txt') as products_file:
+    with open ('./data/products.txt',encoding="utf8") as products_file:
         from_file = products_file.readlines()
 
     products_list = []
@@ -33,8 +33,8 @@ def show_prices(prices):
     
 
     # задати інтервал виводу
-    price_code_from = input("З якого кода продукт? ")
-    price_code_to   = input("По який код продукт? ")
+    price_code_from = input("З якого кода ціна? ")
+    price_code_to   = input("По який код ціна? ")
     
     # накопичує кількість виведених рядків
     kol_lines = 0
@@ -46,8 +46,22 @@ def show_prices(prices):
 
     # перевірити чи був вивід хоча б одного рядка
     if kol_lines == 0:
-        print("По вашому запиту продуктів не знайдено!")
-    
-
+        print("По вашому запиту цін не знайдено!")
 prices = get_prices()
 show_prices(prices)
+
+def show_products(products):
+    product_code_from = input("З якого кода продукт?")
+    product_code_to   = input("По який код продукт?")
+
+    kol_lines = 0
+
+    for product in products:
+        if  product_code_from  <= product[0] <= product_code_to:
+            print("код: {:3} назва продукту: {:3} одиниці вимірювання {:3}".format(product[0],product[1],product[2]))
+            kol_lines += 1
+
+    if kol_lines == 0:
+        print("По вашому запиту продуктів не знайдено!")
+products = get_products()
+show_products(products)
